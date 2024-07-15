173630696ea58246d05e85e1db4cb4a7a3c870e6
mori-akira <77237845+mori-akira@users.noreply.github.com>
2024-07-15 12:34:06 +0900

    Update

--- asset/script.js
+++ asset/script.js
@@ -1,6 +1,3 @@
-const headers = ['資格名', '社内ランク', '提供団体', '取得日', '期限日'];
-const titleFactor = '資格名';
-
 $(async () => {
     const response = await fetch('./data.json');
     const data = await response.json();
@@ -17,11 +14,13 @@ $(async () => {
         'D': 0,
     }
     data.forEach((e, _) => {
+        // 表示対象の項目を絞り込み
         const type = e.filter(f => 'type' === f.type)[0].value;
         if (!targets.includes(type)) {
             return;
         }
 
+        // 期限切れか判定
         let expired = false;
         if (e.filter(f => '期限日' === f.label)[0]) {
             const expiredDate = e.filter(f => '期限日' === f.label)[0].value;
@@ -31,16 +30,23 @@ $(async () => {
             }
         }
 
+        // 一覧の行を追加
         const tr = $(`<tr data-target=${"id_" + index}><td>${index}</td></tr>`);
         if (expired) {
             tr.addClass('expired');
         }
-        headers.forEach(f => {
-            tr.append($(`<td>${e.filter(g => f === g.label)[0]?.value ?? '-'}</td>`));
+        ['資格名', '社内ランク', '提供団体', '取得日', '期限日'].forEach(f => {
+            if (f === '社内ランク') {
+                const rank = e.filter(g => f === g.label)[0]?.value ?? '-';
+                tr.append($(`<td><span class="value rank ${rank}">${rank}</span></td>`));
+            } else {
+                tr.append($(`<td>${e.filter(g => f === g.label)[0]?.value ?? '-'}</td>`));
+            }
         });
         infoTable.append(tr);
 
-        infoList.append($(`<h2 id=${"id_" + index}>${e.filter(f => titleFactor === f.label)[0]?.value ?? '-'}</h2>`));
+        // 詳細の情報を追加
+        infoList.append($(`<h2 id=${"id_" + index}>${e.filter(f => '資格名' === f.label)[0]?.value ?? '-'}</h2>`));
         const div = $('<div class="qualification-info"></div>')
         e.forEach(f => {
             if ('type' === f.type) {
@@ -69,8 +75,9 @@ $(async () => {
         infoList.append(div);
         index++;
 
+        // ランク
         const rank = e.filter(f => '社内ランク' === f.label)[0]?.value;
-        if (rank) {
+        if (rank && !expired) {
             rankCount[rank]++;
         }
     });
